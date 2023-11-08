0c59cdc73b0a4b57aa66db93a0d51e4081205f53
Tatum Sterling <tatum.sterling29@gmail.com>
2023-11-07 19:08:11 -0600

    served up index.html

--- server.js
+++ server.js
@@ -10,11 +10,11 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 // html route
 app.get('/notes', (req, res) =>
-  res.sendFile(path.join(__dirname, 'notes.html'))
+  res.sendFile(path.join(__dirname, './public/notes.html'))
 );
 
 app.get('/', (req, res) =>
-  res.sendFile(path.join(__dirname, 'index.html'))
+  res.sendFile(path.join(__dirname, './public/index.html'))
 );
 // api route
 app.get('/api/notes', (req, res) => {
